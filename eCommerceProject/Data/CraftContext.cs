6c6ae1cd65e09a0195517ebe0683bdfb08ac3600
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-02-14 10:53:15 -0800

    Add CraftContext and update EF Core packages
    Implemented CraftContext class for managing Craft entities and added necessary namespaces. Updated project file to include Entity Framework Core packages for ASP.NET Core diagnostics, SQL Server support, and tools, all set to version 9.0.2.

---
+++ eCommerceProject/Data/CraftContext.cs
@@ -0,0 +1,18 @@
+﻿using eCommerceProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceProject.Data;
+
+public class CraftContext : DbContext
+{
+    /// <summary>
+    /// Constructor for CraftContext
+    /// </summary>
+    /// <param name="options"></param>
+    public CraftContext(DbContextOptions<CraftContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Craft> Crafts { get; set; }
+}
