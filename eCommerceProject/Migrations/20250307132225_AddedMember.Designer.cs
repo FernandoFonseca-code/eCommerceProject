d1f9a88d4a9db1e770be8849c40b395c606b4f85
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-03-07 05:24:40 -0800

    Added migration to member class

--- eCommerceProject/Migrations/20250307132225_AddedMember.Designer.cs
+++ eCommerceProject/Migrations/20250307132225_AddedMember.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eCommerceProject.Data;
 
@@ -10,9 +11,11 @@ using eCommerceProject.Data;
 namespace eCommerceProject.Migrations
 {
     [DbContext(typeof(CraftContext))]
-    partial class CraftContextModelSnapshot : ModelSnapshot
+    [Migration("20250307132225_AddedMember")]
+    partial class AddedMember
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
