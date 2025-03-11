c29811d12186fc0ef67b95f6a3e9ff7bc6340ade
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-03-11 13:15:26 -0700

    Add CartController and update redirection logic
    - Introduced `CartController` to manage cart actions, including adding crafts.
    - Updated `CraftController` to redirect to "Add" after craft updates and deletions.
    - Modified `MembersController` to redirect to "Add" after member actions.
    - Added "Add to Cart" button in `Index.cshtml` for improved user interaction.

---
+++ eCommerceProject/Controllers/CartController.cs
@@ -0,0 +1,31 @@
+﻿using eCommerceProject.Data;
+using eCommerceProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceProject.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CraftContext _context;
+
+        public CartController(CraftContext context )
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Craft? craftToCart = _context.Crafts.Where(c => c.CraftId == id).SingleOrDefault();
+
+            if (craftToCart == null)
+            {
+
+                TempData["Message"] = "Apologies, that craft is no longer exists";
+                return RedirectToAction("Index", "Craft");
+            }
+            
+            TempData["Message"] = $"{craftToCart.Title} was added to your cart";
+            return RedirectToAction("Index", "Craft");
+        }
+    }
+}
