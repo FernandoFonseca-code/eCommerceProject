200cd27d1394eceaaa19c0e6a42c3de4c451e2a2
FernandoFonseca-code <fernando.fonseca2021@gmail.com>
2025-03-11 06:45:17 -0700

    log out functionality completed

--- eCommerceProject/Controllers/MembersController.cs
+++ eCommerceProject/Controllers/MembersController.cs
@@ -25,15 +25,16 @@ public class MembersController : Controller
     {
         if (ModelState.IsValid)
         {
-            /// Map RegisterViewModel data to Member object
-            Member member = new()
+            // Map RegisterViewModel data to Member object
+            Member newMember = new()
             {
                 Email = regModel.Email,
                 Password = regModel.Password
             };
 
-            _context.Members.Add(member);
+            _context.Members.Add(newMember);
             await _context.SaveChangesAsync();
+            LogUserIn(newMember.Email);
 
             return RedirectToAction("Index", "Home");
         }
@@ -51,22 +52,22 @@ public class MembersController : Controller
     {
         if (ModelState.IsValid)
         {
-            /// Check if the member exists in the database
-            /// LINQ query to check if the member exists in the database
+            // Check if the newMember exists in the database
+            // LINQ query to check if the newMember exists in the database
             Member? member = await _context.Members
                 .Where(m => m.Email == loginModel.Email && m.Password == loginModel.Password)
                 .FirstOrDefaultAsync();
 
-            /// Traditional SQL query to check if the member exists in the database
-            /// Member? m = await (from member in _context.Members
-            ///                   where member.Email == loginModel.Email &&
-            ///                          member.Password == loginModel.Password
-            ///                   select member).SingleOrDefaultAsync();
+            // Traditional SQL query to check if the newMember exists in the database
+            // Member? m = await (from newMember in _context.Members
+            //                   where newMember.Email == loginModel.Email &&
+            //                          newMember.Password == loginModel.Password
+            //                   select newMember).SingleOrDefaultAsync();
 
-            /// If the member exists, redirect to the home page
+            // If the newMember exists, redirect to the home page
             if (member != null)
             {
-                HttpContext.Session.SetString("Email", loginModel.Email);
+                LogUserIn(loginModel.Email);
                 return RedirectToAction("Index", "Home");
             }
             else
@@ -75,5 +76,19 @@ public class MembersController : Controller
             }
         }
         return View(loginModel);
+    }
+    /// <summary>
+    /// This method logs the user in by setting the email value in the session object
+    /// </summary>
+    /// <param name="email"></param>
+    private void LogUserIn(string email)
+    {
+        HttpContext.Session.SetString("Email", email);
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Remove("Email");
+        return RedirectToAction("Index", "Home");
     }
 }
